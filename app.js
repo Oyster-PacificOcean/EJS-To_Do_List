b8ffef3b75484c206de63aaa3936a739566d461e
Oyster-PacificOcean <79025005+Oyster-PacificOcean@users.noreply.github.com>
2021-12-24 21:06:17 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+
+const day = date.getDate();
+
+  res.render("list", {
+    listTitle: day,
+    newListItems: items
+  });
+
+});
+
+app.post("/", function(req, res) {
+  const item = req.body.newItem;
+
+if (req.body.list === "Work") {
+  workItems.push(item);
+  res.redirect("/work");
+} else {
+  items.push(item);
+  res.redirect("/");
+}
+
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+})
+
+// app.post("/work", function(req, res) {
+//   let item = req.body.newItem;
+//   workItems.push(item);
+//   res.redirect("/work");
+// });
+
+// var currentDay = today.getDay();
+// var day = "";
+
+// switch (currentDay) {
+//   case 0:
+//     day = "Sunday";
+//     break;
+//   case 1:
+//     day = "Monday";
+//     break;
+//   case 2:
+//     day = "Tuesday";
+//     break;
+//   case 3:
+//     day = "Wednesday";
+//     break;
+//   case 4:
+//     day = "Thursday";
+//     break;
+//   case 5:
+//     day = "Friday";
+//     break;
+//   case 6:
+//     day = "Saturday";
+//     break;
+//
+//   default:
+//   console.log("Error: current day is equal to: " + currentDay);
+// }
+
+// if (currentDay === 6 || currentDay === 0) {
+//   day = "Weekend";
+//   // res.render("list", {kindOfDay: day});
+//   // res.sendFile(__dirname + "/weekend.html");
+//   // res.write("<h1>Yay it's the weekend!</h1>");
+// } else {
+//   day = "Weekday";
+//   // res.render("list", {kindOfDay: day});
+//   // res.sendFile(__dirname + "/weekday.html");
+//   // res.write("<p>It is not the weekend.</p>");
+//   // res.write("<h1>Boo! I have to work!</h1>");
+//   // res.send();
+// }
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
